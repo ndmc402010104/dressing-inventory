f5fc404efdae2e69d3a8ceb3a44e144069dbdf35
益昇 石 <ndmc402010104@gmail.com>
2026-06-14 17:05:11 +0800

    chore: update dressing-inventory runtime/version v0.0.3-202606141705

--- version.js
+++ version.js
@@ -1,10 +1,10 @@
 window.SKHPS_VERSION = {
   appId: "dressing-inventory",
-  version: "v0.0.2-202606141703",
+  version: "v0.0.3-202606141705",
   major: 0,
   minor: 0,
-  patch: 2,
-  buildTime: "202606141703",
-  updatedAt: "2026-06-14T17:03:00+08:00",
+  patch: 3,
+  buildTime: "202606141705",
+  updatedAt: "2026-06-14T17:05:00+08:00",
   source: "version.js"
 };
